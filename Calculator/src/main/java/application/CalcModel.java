912a3f47d88b2eb0ff1f4f783982a1279049bac6
Dennis Marjanov <ZLAC346@live.rhul.ac.uk>
2023-11-24 08:18:22 +0000

    CalcModel class has been updated to fit the specification and matches the inital code provided in  calculator class

--- Calculator/src/main/java/application/CalcModel.java
+++ Calculator/src/main/java/application/CalcModel.java
@@ -4,8 +4,20 @@ package application;
  * Evaluates an expression - the evaluation can be Standard (infix) or reverse polish.
  */
 public class CalcModel implements Calculator {
+  private Calculator revPolish;
+  private Calculator standard;
+
+  public CalcModel(Calculator revPolish, Calculator standard) {
+    this.revPolish = revPolish;
+    this.standard = standard;
+  }
+
   @Override
   public float evaluate(String expression, Boolean infix) throws InvalidExpression {
-    return 0.0f;
+    if (infix) {
+      return standard.evaluate(expression, infix);
+    } else {
+      return revPolish.evaluate(expression, infix);
+    }
   }
 }
