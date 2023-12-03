76b8c8e7d46a7ef255820ed0b8c988bbdaa5d299
Dennis Marjanov <ZLAC346@live.rhul.ac.uk>
2023-12-03 13:58:10 +0000

    Evaulation of string no longer done by hard code in method , now it is done by the ReversePolishCalculator when converted to postfix

--- Calculator/src/main/java/application/StandardCalc.java
+++ Calculator/src/main/java/application/StandardCalc.java
@@ -15,73 +15,76 @@ public class StandardCalc implements Calculator {
 
   @Override
   public float evaluate(String expression, Boolean infix) throws InvalidExpression {
-    if (!infix) {
-      // Implementation for reverse polish notation will be added later
-      return 0; // Placeholder
-    }
-
-    // Split the expression into operands and operator
-    String[] tokens = expression.split("\\s+");
-    float operand1 = Float.parseFloat(tokens[0]);
-    String operator = tokens[1];
-    float operand2 = Float.parseFloat(tokens[2]);
-
-    // Perform the arithmetic operation
-    switch (operator) {
-      case "+":
-        return operand1 + operand2;
-      case "-":
-        return operand1 - operand2;
-      case "*":
-        return operand1 * operand2;
-      case "/":
-        if (operand2 == 0) {
-          throw new InvalidExpression();
-        }
-        return operand1 / operand2;
-      default:
-        throw new InvalidExpression();
+    if (infix) {
+      // Convert infix to postfix and then evaluate using rpCalc
+      String postfixExpression = convertToPostfix(expression);
+      return rpCalc.evaluate(postfixExpression);
+    } else {
+      return 0;
     }
   }
 
-  String convertToPostfix(String infix) throws InvalidExpression {//Trail 1 Shunting yard Algorit
-    StringBuilder postfix = new StringBuilder();
-    Deque<Character> stack = new ArrayDeque<>();
+  String convertToPostfix(String infix) throws InvalidExpression {
+    StringBuilder postfix = new StringBuilder();// StringBuilder to build the resulting postfix
+                                                // expression
+    Deque<String> stack = new ArrayDeque<>();// Stack to store operators during the conversion
 
+    // Loop through each character in the infix expression
     for (int i = 0; i < infix.length(); i++) {
       char c = infix.charAt(i);
 
+      // Ignore whitespaces
       if (Character.isWhitespace(c)) {
         continue;
       }
 
-      if (Character.isDigit(c)) {
-        postfix.append(c).append(' ');
+      // If the character is a digit or a dot, it's part of a numeric value
+      if (Character.isDigit(c) || c == '.') {
+        // Handle numeric values (including decimal points)
+        StringBuilder numericValue = new StringBuilder();
+        numericValue.append(c);
+
+        // Continue to append digits and dots until a non-digit character is encountered
+        while (i + 1 < infix.length()
+            && (Character.isDigit(infix.charAt(i + 1)) || infix.charAt(i + 1) == '.')) {
+          numericValue.append(infix.charAt(++i));
+        }
+
+        // Append the complete numeric value to the postfix expression
+        postfix.append(numericValue.toString()).append(' ');
       } else {
-        while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(c)) {
+        // If the character is an operator
+        while (!stack.isEmpty() && precedence(stack.peek()) >= precedence(String.valueOf(c))) {
+          // Pop and append operators from the stack to the postfix expression
           postfix.append(stack.pop()).append(' ');
         }
-        stack.push(c);
+        // Push the current operator onto the stack
+        stack.push(String.valueOf(c));
       }
     }
 
+    // Pop and append any remaining operators from the stack to the postfix expression
     while (!stack.isEmpty()) {
       postfix.append(stack.pop()).append(' ');
     }
 
+    // Trim and return the final postfix expression
     return postfix.toString().trim();
   }
 
-  private int precedence(char op) {// order in which operators are evaluated in an expression
+
+
+  int precedence(String op) {
     switch (op) {
-      case '+':
-      case '-':
+      case "+":
+      case "-":
         return 1;
-      case '*':
-      case '/':
+      case "*":
+      case "/":
         return 2;
       default:
         return 0;
     }
   }
+
 }
--- Calculator/src/test/java/application/StandardCalcTest.java
+++ Calculator/src/test/java/application/StandardCalcTest.java
@@ -28,8 +28,6 @@ public class StandardCalcTest {
     @Test
     void testConvertToPostfixBasic() throws InvalidExpression {
         StandardCalc calculator = new StandardCalc();
-
-        // Test a basic infix expression
         assertEquals("3 4 +", calculator.convertToPostfix("3 + 4"));
         assertEquals("5 2 -", calculator.convertToPostfix("5 - 2"));
         assertEquals("2 3 *", calculator.convertToPostfix("2 * 3"));
@@ -39,13 +37,24 @@ public class StandardCalcTest {
     @Test
     void testConvertToPostfixDifferentValues() throws InvalidExpression {
         StandardCalc calculator = new StandardCalc();
-
-        // Test infix expressions with different numeric values
         assertEquals("3.5 4.2 +", calculator.convertToPostfix("3.5 + 4.2"));
         assertEquals("1.2 2.5 -", calculator.convertToPostfix("1.2 - 2.5"));
         assertEquals("2.5 3.1 *", calculator.convertToPostfix("2.5 * 3.1"));
         assertEquals("7.0 2.0 /", calculator.convertToPostfix("7.0 / 2.0"));
     }
     
+    @Test
+    void testSimpleDivision() throws InvalidExpression {
+        Calculator calculator = new StandardCalc();
+        float result = calculator.evaluate("10 / 2", true);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void testSimpleSubtraction() throws InvalidExpression {
+        Calculator calculator = new StandardCalc();
+        float result = calculator.evaluate("8 - 3", true);
+        assertEquals(5, result);
+    }
    
 }
