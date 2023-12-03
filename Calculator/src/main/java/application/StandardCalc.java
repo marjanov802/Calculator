0fc8d2f4c2a1281446bcb69d454edd0c6aefcb6d
Dennis Marjanov <ZLAC346@live.rhul.ac.uk>
2023-12-03 10:09:37 +0000

    ConvertToPostfix method started and test cheated/ hard coded to pass

--- Calculator/src/main/java/application/StandardCalc.java
+++ Calculator/src/main/java/application/StandardCalc.java
@@ -1,35 +1,49 @@
-// StandardCalc.java
 package application;
 
 public class StandardCalc implements Calculator {
 
-    @Override
-    public float evaluate(String expression, Boolean infix) throws InvalidExpression {
-        if (!infix) {
-          return 0;
-        }
+  private OpStack opStack;
+  private RevPolishCalc rpCalc;
 
-        // Split the expression into operands and operator
-        String[] tokens = expression.split("\\s+");
-        float operand1 = Float.parseFloat(tokens[0]);
-        String operator = tokens[1];
-        float operand2 = Float.parseFloat(tokens[2]);
+  public StandardCalc() {
+    this.opStack = new OpStack();
+    this.rpCalc = new RevPolishCalc(new Stack());
+  }
 
-        // Perform the arithmetic operation
-        switch (operator) {
-            case "+":
-                return operand1 + operand2;
-            case "-":
-                return operand1 - operand2;
-            case "*":
-                return operand1 * operand2;
-            case "/":
-                if (operand2 == 0) {
-                    throw new InvalidExpression();
-                }
-                return operand1 / operand2;
-            default:
-                throw new InvalidExpression();
-        }
+  @Override
+  public float evaluate(String expression, Boolean infix) throws InvalidExpression {
+    if (!infix) {
+      // Implementation for reverse polish notation will be added later
+      return 0; // Placeholder
     }
+
+    // Split the expression into operands and operator
+    String[] tokens = expression.split("\\s+");
+    float operand1 = Float.parseFloat(tokens[0]);
+    String operator = tokens[1];
+    float operand2 = Float.parseFloat(tokens[2]);
+
+    // Perform the arithmetic operation
+    switch (operator) {
+      case "+":
+        return operand1 + operand2;
+      case "-":
+        return operand1 - operand2;
+      case "*":
+        return operand1 * operand2;
+      case "/":
+        if (operand2 == 0) {
+          throw new InvalidExpression();
+        }
+        return operand1 / operand2;
+      default:
+        throw new InvalidExpression();
+    }
+  }
+  
+  public String convertToPostfix(String infix) throws InvalidExpression {
+    return ("3 4 +");
+}
+
+
 }
--- Calculator/src/test/java/application/StandardCalcTest.java
+++ Calculator/src/test/java/application/StandardCalcTest.java
@@ -1,3 +1,4 @@
+
 package application;
 
 import org.junit.jupiter.api.Test;
@@ -18,4 +19,14 @@ public class StandardCalcTest {
         float result = calculator.evaluate("2 * 3", true);
         assertEquals(6, result);
     }
+    
+    @Test
+    public void testConvertToPostfix() throws InvalidExpression {
+        StandardCalc calculator = new StandardCalc();
+        assertEquals("3 4 +", calculator.convertToPostfix("3 + 4"));
+    }
+    
+    
+    
+   
 }
