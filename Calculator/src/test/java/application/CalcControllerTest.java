15f681277f3db78bf5f219f7aa2e9158eaef76da
Dennis Marjanov <ZLAC346@live.rhul.ac.uk>
2023-12-04 11:55:23 +0000

    Made test class for CalcController

---
+++ Calculator/src/test/java/application/CalcControllerTest.java
@@ -0,0 +1,13 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CalcControllerTest {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
