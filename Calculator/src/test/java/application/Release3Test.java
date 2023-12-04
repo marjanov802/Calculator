6602c0bc5b1ddf7b6241fe25fb6c501e22a37e01
Dennis Marjanov <ZLAC346@live.rhul.ac.uk>
2023-12-04 20:10:51 +0000

    Testing on release 3 complete

--- Calculator/src/test/java/application/Release3Test.java
+++ Calculator/src/test/java/application/Release3Test.java
@@ -19,6 +19,24 @@ class Release3Test {
       assertEquals(14.0, result);
   }
   
+  @Test
+  void testInvalidInfixExpression() {
+      Calculator calculator = new StandardCalc();
+      assertThrows(InvalidExpression.class, () -> calculator.evaluate("2 + * 3", true));
+  }
   
+  @Test
+  void testInvalidPostfixExpression() {
+      Calculator calculator = new StandardCalc();
+      assertThrows(InvalidExpression.class, () -> calculator.evaluate("2 3 * *", false));
+  }
+  
+  @Test
+  void testCalcModelEvaluateExpression() throws InvalidExpression {
+      RevPolishCalc rpCalc = new RevPolishCalc(new Stack());
+      CalcModel calcModel = new CalcModel(rpCalc);
+      float result = calcModel.evaluateExpression("2 3 4 * +");
+      assertEquals(14.0, result);
+  }
   
 }
