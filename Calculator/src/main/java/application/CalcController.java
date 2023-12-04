7f5af7fbbd440a44b510122989e4122f2f8981d3
Dennis Marjanov <ZLAC346@live.rhul.ac.uk>
2023-12-04 11:41:21 +0000

    Attributes and methods added as per UML diagram for CalcController

--- Calculator/src/main/java/application/CalcController.java
+++ Calculator/src/main/java/application/CalcController.java
@@ -7,10 +7,22 @@ package application;
 public class CalcController {
   private CalcModel myModel;
   private ViewInterface myView;
+  private boolean isInFix;
 
   private void handleCalculation() {}
 
-  private void handleTypeChange() {}
+  private void handleTypeChange(OpType opType) {
+    isInFix = (opType == OpType.STANDARD); 
 
-  CalcController(CalcModel model, ViewInterface view) {}
+  }
+
+  CalcController(CalcModel model, ViewInterface view) {
+    this.myModel = model;
+    this.myView = view;
+    this.isInFix = true;
+    
+ // Register the controller as an observer to the view
+    myView.addCalculateObserver(this::handleCalculation);
+    myView.addTypeObserver(this::handleTypeChange);
+  }
 }
