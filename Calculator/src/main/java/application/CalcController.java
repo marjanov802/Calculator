8301c2e9bddb2c688fb84f7219795549a9f18983
Dennis Marjanov <ZLAC346@live.rhul.ac.uk>
2023-12-07 12:23:21 +0000

    Added javaDoc to satisfy maven tests

--- Calculator/src/main/java/application/CalcController.java
+++ Calculator/src/main/java/application/CalcController.java
@@ -1,11 +1,22 @@
 package application;
 
+/**
+ * Controller for Calculator that makes connection between controller and view.
+ * @param model The calculator model for evaluating expressions.
+ * @param view  The user interface view for user interactions and displaying results.
+ */
 public class CalcController {
 
   private CalcModel model;
   private ViewInterface view;
   private OpType currentOpType;
 
+  /**
+   * Constructs a new CalcController with the specified CalcModel and ViewInterface.
+   *
+   * @param model The CalcModel responsible for evaluating expressions.
+   * @param view  The ViewInterface represents user interface for user interactions.
+   */
   public CalcController(CalcModel model, ViewInterface view) {
     this.model = model;
     this.view = view;
